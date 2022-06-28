70bc295b6922fb47dafb56b3971fbc85c25b5ab9
thebestcoder12 <87145299+thebestcoder12@users.noreply.github.com>
2022-06-28 12:44:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function preload() {
+    classifier = ml5.imageClassifier('DoodleNet')
+}
+
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+
+function clearCanvas() {
+
+    background("white");
+}
+
+function draw() {
+    strokeWeight(13);
+    stroke("black");
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX.mouseY);
+    }
+
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById("label").innerHTML = 'label: ' + results[0].label;
+    document.getElementById("confindence").innerHTML = 'Confidence: ' + Math.round(results[0].confindence * 100)+ '%';
+
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
